7588210774720c79a643cdb2eb15d8f18d3e7f0d
Jean-François Roche <jfroche@affinitic.be>
2012-11-08 16:40:02 +0100

    remove subtyper

--- gites/calendar/setuphandlers.py
+++ gites/calendar/setuphandlers.py
@@ -10,8 +10,7 @@ $Id: event.py 67630 2006-04-27 00:54:03Z jfroche $
 from zope.interface import alsoProvides
 from gites.core.utils import (createFolder, createPage, getManager)
 from gites.calendar.interfaces import IProprioCalendar
-from zope.component import getUtility, getMultiAdapter
-from p4a.subtyper.interfaces import ISubtyper
+from zope.component import getMultiAdapter
 from plone.portlets.interfaces import IPortletAssignmentMapping
 import logging
 from gites.calendar.portlets import calendarmenu
@@ -19,11 +18,7 @@ logger = logging.getLogger('gites.calendar')
 
 
 def setupCalendar(calendarFolder):
-
-    subtyper = getUtility(ISubtyper)
-    existing = subtyper.existing_type(calendarFolder)
-    if existing is None or existing.name != 'p4a.plonecalendar.FolderCalendar':
-        subtyper.change_type(calendarFolder, 'p4a.plonecalendar.FolderCalendar')
+    return
 
 
 def setupPortletsInZoneMembre(folder):
